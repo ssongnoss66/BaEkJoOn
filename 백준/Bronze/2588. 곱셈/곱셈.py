0b6005ccfc102247d13dd581d4fd8fc0845cf4dd
SSONGNOSS <121418205+ssongnoss66@users.noreply.github.com>
2023-01-03 22:57:34 +0900

    [Bronze III] Title: 곱셈, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+a, b, c = map(int, input())
+"""
+num1 = 472
+a = 3
+b = 8
+c = 5
+"""
+print(num1*c)
+print(num1*b)
+print(num1*a)
+print((num1*c)+((num1*b)*10)+((num1*a)*100))
