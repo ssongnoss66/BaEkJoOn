418fbbfafb6e73bccf7874c964c5c117b977aab4
SSONGNOSS <121418205+ssongnoss66@users.noreply.github.com>
2023-01-26 17:56:06 +0900

    [Silver IV] Title: 한수, Time: 36 ms, Memory: 30748 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,16 @@
+def hansu(num):
+    cnt = 99
+    numB = 100
+    if num < 100:
+        return(num)
+    else:
+        while True:
+            if (numB//100) - ((numB % 100) // 10) == ((numB % 100) // 10) - ((numB % 100) % 10):
+                cnt += 1
+            if numB == num:
+                break
+            numB += 1
+        return(cnt)
+                
+N = int(input())
+print(hansu(N))
