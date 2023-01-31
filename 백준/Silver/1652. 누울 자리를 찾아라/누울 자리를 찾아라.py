2d591913dff971e27de89c46f406414abc787612
SSONGNOSS <121418205+ssongnoss66@users.noreply.github.com>
2023-01-31 21:37:12 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -1,26 +1,21 @@
+# internetVer
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-board = [list(input().rstrip()) for _ in range(n)]
+arr = [input().rstrip() for _ in range(n)]
+# print(f"arr {arr}")
+arr_h = [''.join(i) for i in zip(*arr)]
+# print(f"arr_h {arr_h}")
+h, v = 0, 0
 
-ans = [0,0]
-for i in range(n):
-    leng_r,leng_c = 0,0
-    for j in range(n):
-        # 가로
-        if board[i][j] == '.':
-            leng_r+=1
-        else:
-            leng_r=0
-        if leng_r==2:
-            ans[0] += 1
-        
-        # 세로
-        if board[j][i] == '.':
-            leng_c+=1
-        else:
-            leng_c=0
-        if leng_c==2:
-            ans[1] += 1
-print(*ans)
+for i in range(n): 
+    for j in arr[i].split('X'):
+        if '..' in j:
+            h += 1
+    for j in arr_h[i].split('X'):
+        if '..' in j:
+            v += 1
+
+print(h,v)
